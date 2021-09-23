e97895e8345209841e835fd82d0956df8f985a24
Dat Tran <dat.tran@ghielectronics.com>
2021-09-23 14:20:03 -0400

    Auto-push

---
+++ monacoworker.js
@@ -0,0 +1,5 @@
+// see https://github.com/Microsoft/monaco-editor
+self.MonacoEnvironment = { 
+    baseUrl: "/brainpad-classic/" 
+};
+importScripts("/brainpad-classic/vs/base/worker/workerMain.js");
