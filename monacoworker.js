4f7e576ef8a0f7df7f094bceb721ae85986858f9
Dat Tran <dat.tran@ghielectronics.com>
2021-10-08 09:16:33 -0400

    Initial.

--- monacoworker.js
+++
@@ -1,5 +0,0 @@
-// see https://github.com/Microsoft/monaco-editor
-self.MonacoEnvironment = { 
-    baseUrl: "/brainpad-classic/" 
-};
-importScripts("/brainpad-classic/vs/base/worker/workerMain.js");
